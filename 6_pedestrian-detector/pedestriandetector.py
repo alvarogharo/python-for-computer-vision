bdfeb183773df62687eba3e6a3693ac8d7e002cc
Alvaro <alvarogharo@gmail.com>
2019-10-25 18:17:52 +0200

    Finished pedestrian detector

--- 6_pedestrian-detector/pedestriandetector.py
+++ 6_pedestrian-detector/pedestriandetector.py
@@ -2,52 +2,34 @@ import os
 import argparse
 import cv2
 
-faceCascadePath = "./classifiers/haarcascade_frontalface_default.xml"
-eyeCascadePath = "./classifiers/haarcascade_eye.xml"
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--images", required = True, help = "Path to where the images resides")
 ap.add_argument("-o", "--out", required = True, help = "Path to where the video will be stored")
 args = vars(ap.parse_args())
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-
-path = args["video"]
-output_folder = args["video"]
+path = args["images"]
+output_folder = args["out"]
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-num_images = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+imagesPaths = os.listdir(path)
+imagesPaths.sort()
 
-for i in range(num_images):
-    images.append(Image.open(path + "/" + image_prefix + str(i) + image_suffix))
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-out = cv2.VideoWriter(output_folder + name + ".mp4", fourcc, 33.0, (640, 480))
 
-cap = cv2.VideoCapture(path)
-while cap.isOpened():
-    ret, frame = cap.read()
+count = 0
+for imagePath in imagesPaths:
+    image = cv2.imread(path + "/" + imagePath)
 
-    faceCascade = cv2.CascadeClassifier(faceCascadePath)
-    rects_face = faceCascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    rects, weights = hog.detectMultiScale(image, winStride=(8, 8), padding=(32, 32), scale=1.05)
 
-    eyeCascade = cv2.CascadeClassifier(eyeCascadePath)
-    rects_eye = eyeCascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    for r in rects:
+        cv2.rectangle(image, (r[0], r[1]), (r[0]+r[2], r[1]+r[3]), (0, 255, 0), 3)
 
-    for r in rects_face:
-        cv2.rectangle(frame, (r[0], r[1]), (r[0]+r[2], r[1]+r[3]), (0, 255, 0), 3)
+    cv2.imwrite(output_folder + "/" + imagePath, image)
+    count += 1
+    print(str(round((count/len(imagesPaths))*100), 2) + "%")
 
-    for r in rects_eye:
-        cv2.rectangle(frame, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), (255, 0, 0), 3)
-
-    out.write(frame)
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(20) & 0xFF == ord("q"):
-        break
-
-cap.release()
-out.release()
-cv2.destroyAllWindows()
